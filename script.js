942cb49d28fa979243d7d838fdf2b0411a34100a
Sara Teshome <tes-sar@hotmail.com>
2022-04-03 09:35:43 -0700

    code changes

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+let btn1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2"); 
+let btn3 = document.querySelector("#btn3"); 
+
+btn1.addEventListener("click" , () => {
+    document.body.style.backgroundImage = "url('cityp.jpg')";
+})
